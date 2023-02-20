bb12c5e9e2b8f6f84f0988ab5e9f5b38395a0f7a
Bakar002 <abubakaramjad78600@gmail.com>
2023-02-20 18:36:42 +0500

    Completed front interface

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardsData from './CardsData';
+import "./style.css"
 
 const Cards = () => {
+    const [data,setData]=useState(CardsData);
+   
   return (
-    <div>Cards</div>
-  )
+    <div className="container mt-3">
+        <h2 className='text-center'>
+            Add to Cart Projects
+        </h2>
+<div className="row d-flex justify-content-center allign-item-center">
+{
+    data.map((element,id)=>{
+return(<>
+<Card style={{ width: '22rem',border:"none" }} className="mx-2 mt-4 card_style">
+      <Card.Img variant="top" src={element.imgdata} style={{height:"16rem"}}className="mt-3" />
+      <Card.Body>
+        <Card.Title>{element.rname}</Card.Title>
+        <Card.Text>
+Price: ₨ {element.price}
+        </Card.Text>
+        <div className="button_div d-flex justify-content-center allign-item-center">
+        <Button variant="primary"className='col-lg-12'>Add to Cart</Button>
+        </div>
+        
+      </Card.Body>
+    </Card></>
+)
+    })
 }
 
+
+</div>
+        
+    </div>
+  )
+}
 export default Cards
